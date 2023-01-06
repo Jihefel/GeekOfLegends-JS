bedd6930af358d7c0d05ff8fe440a8d991062f05
Jihefel <jfl.jflament@gmail.com>
2023-01-06 23:17:50 +0100

    Amélioration de la méthode enigme + condition des 20% de PV du boss

--- public/js/modules/functions.js
+++ public/js/modules/functions.js
@@ -25,9 +25,9 @@ export function randomArrows(min, max) {
 // Enigmes aléatoires
 export function randomEnigme(bossChoisi) {
     // Boss en dessous de 20% de pv
-    // if (bossChoisi.pv <= (bossChoisi.pv)*0.2) {
+    if (bossChoisi.pv <= (bossChoisi.pv)*0.2) {
         // Enigmes possibles
-        let enigmesPrompt = [`Que vaut le résultat des index 2,5,3,8,7 du mot "refluates"`, `Je transforme une plante en une planète. Qui suis-je ?`, `Un père et son fils ont 36 ans à eux deux. Le père a 30 ans de plus que son fils.`]
+        let enigmesPrompt = [`Que vaut le résultat des index 2,5,3,8,7 du mot "refluates"`, `Je transforme une plante en une planète. Qui suis-je ?`, `Un père et son fils ont 36 ans à eux deux. Le père a 30 ans de plus que son fils. Quel est l'âge du père ?`]
         // Réponses aux énigmes
         let enigmesReponse = ["false", "è", 33]
         // Enigme random demandée
@@ -38,16 +38,16 @@ export function randomEnigme(bossChoisi) {
             let reponseUser = prompt(enigmeAsked)/*Bonus =>*/.toLocaleLowerCase().replace(/\s/g, '').trim();
             console.log(reponseUser);
             if (reponseUser === enigmesReponse[enigmesPrompt.indexOf(enigmeAsked)] /*Bonus 2 =>*/|| reponseUser.includes(enigmesReponse[enigmesPrompt.indexOf(enigmeAsked)]) === true) {
-                    // Mort du boss
-                    bossChoisi.pv = 0;
-                    console.log(`Vous avez réussi à tuer ${bossChoisi.nom} !`);
-                    break;
+                // Mort du boss
+                bossChoisi.pv = 0;
+                console.log(`Vous avez réussi à tuer ${bossChoisi.nom} !`);
+                break;
             } else {
                 // Faux, encore "x" essais
                 nbFoisQuestion++;
                 if (nbFoisQuestion < 2) {
-                console.log(`Faux. Il vous reste ${(3 - nbFoisQuestion)} essais`);
-                } else {
+                    console.log(`Faux. Il vous reste ${(3 - nbFoisQuestion)} essais`);
+                } else if (nbFoisQuestion == 2) {
                     console.log(`Faux. Il vous reste ${(3 - nbFoisQuestion)} essai`);
                 }
             }
@@ -60,4 +60,4 @@ export function randomEnigme(bossChoisi) {
             console.log("Vous avez échoué. Tous vos héros sont morts");
         }
     }
-// }
+}
